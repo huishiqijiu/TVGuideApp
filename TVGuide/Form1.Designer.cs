9aef1a218d1d05979aa8ad77853c66c41bf6857e
huishiqijiu <huishi0709@gmail.com>
2016-06-20 20:36:15 +0200

    First commit

---
+++ TVGuide/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TVGuide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxChannels = new System.Windows.Forms.ComboBox();
+            this.comboBoxDay = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxChannels
+            // 
+            this.comboBoxChannels.FormattingEnabled = true;
+            this.comboBoxChannels.Location = new System.Drawing.Point(138, 208);
+            this.comboBoxChannels.Name = "comboBoxChannels";
+            this.comboBoxChannels.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxChannels.TabIndex = 0;
+            // 
+            // comboBoxDay
+            // 
+            this.comboBoxDay.FormattingEnabled = true;
+            this.comboBoxDay.Location = new System.Drawing.Point(138, 110);
+            this.comboBoxDay.Name = "comboBoxDay";
+            this.comboBoxDay.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDay.TabIndex = 1;
+            this.comboBoxDay.SelectedIndexChanged += new System.EventHandler(this.comboBoxDay_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(323, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(205, 650);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(598, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 183);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Channel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Programs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(595, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 719);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxDay);
+            this.Controls.Add(this.comboBoxChannels);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxChannels;
+        private System.Windows.Forms.ComboBox comboBoxDay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TVGuide/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TVGuide
+{
+    public partial class Form1 : Form
+    {
+        public static string ScreenScrape(string url)
+        {
+            return new System.Net.WebClient().DownloadString(url);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            List<DateTime> weekdays = new List<DateTime>();
+            weekdays.Add(DateTime.Now);
+            weekdays.Add(DateTime.Now.AddDays(1));
+            weekdays.Add(DateTime.Now.AddDays(2));
+            weekdays.Add(DateTime.Now.AddDays(3));
+            weekdays.Add(DateTime.Now.AddDays(-1));
+            weekdays.Add(DateTime.Now.AddDays(-2));
+            weekdays.Sort();
+            foreach (var v in weekdays)
+            {
+                comboBoxDay.Items.Add(v.ToString("yyyy-MM-dd"));
+            }
+
+        }
+        string[] progPartArray;
+        string programPart;
+        string all;
+        string date;
+        private void comboBoxDay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            date = comboBoxDay.Text;
+            all = ScreenScrape("http://www.tv.nu/" + date);
+            int start = all.IndexOf("<li class=\"channel-schedule");
+            int end = all.LastIndexOf("</li>			</ol>");
+            int length = end - start;
+            programPart = all.Substring(start, length);
+            progPartArray = programPart.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            List<String> channels = new List<string>();
+            foreach (var v in progPartArray)
+            {
+                if (v.StartsWith("data-slug="))
+                {
+                    var name = v.Substring(v.IndexOf("="));
+                    channels.Add(name);
+                }
+            }
+            List<String> cleanNames = new List<string>();
+            foreach (var v in channels)
+            {
+                int s = 2;
+                int nd = v.Length - 2;
+                int l = nd - s;
+                var name = v.Substring(s, l);
+                cleanNames.Add(name);
+                comboBoxChannels.Items.Add(name);
+            }
+        }
+        List<string> ids = new List<string>();
+        string progId;
+        string[] programsInChannel2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            ids.Clear();
+            List<string> programList = new List<string>();
+            var channel = comboBoxChannels.Text;
+            date = comboBoxDay.Text;
+            string url = "http://www.tv.nu/kanal/" + channel + '/' + date; 
+            string all2 = ScreenScrape(url);
+            progPartArray = all2.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);           
+            foreach (var v in progPartArray)
+            {
+                if (v.StartsWith("data-id="))
+                {
+                    string id = v.Substring(v.IndexOf("="));
+                    string cleanId = id.Substring(2, id.Length - 2).Trim();
+                    string cleanId2 = cleanId.Remove(cleanId.Length - 1);
+                    
+                    if (cleanId2.Length>5)
+                    ids.Add(cleanId2);
+                }
+            }
+            var templist = ids;
+            string[] sep2 = new string[] { "data-id=\"" };
+            string[] programsInChannel = all2.Split(sep2, StringSplitOptions.None);
+            programsInChannel2 = programsInChannel.Where(w => w != programsInChannel[0]).ToArray();
+            foreach (var p in programsInChannel2)
+            {
+                int s1 = p.IndexOf("data-title=\"") + 12;
+                int e1 = p.IndexOf("data-start-time=\"");
+                int l1 = e1 - s1;
+                string programName = p.Substring(s1, l1).Trim().Remove((p.Substring(s1, l1).Trim()).Length - 1);
+                var convertedProgramName = Encoding.UTF8.GetString(Encoding.Default.GetBytes(programName));
+                int timeStart = e1 + 28;
+                string time = p.Substring(timeStart, 5).Trim();
+               
+                programList.Add(time + " " + convertedProgramName);
+                
+            }
+            foreach (var prog in programList)
+            {
+                listView1.Items.Add(prog);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (listView1.SelectedItems.Count > 0)
+            {
+               
+                int index = listView1.SelectedIndices[0];
+                progId = ids[index];
+                if (!Char.IsNumber(progId[progId.Length - 1]) && !Char.IsLetter(progId[progId.Length - 1]))
+                    progId = progId.Remove(progId.Length - 1);
+                string detailInfo = ScreenScrape("http://www.tv.nu/p/" + progId);
+                int start = detailInfo.IndexOf("Om programmet");
+                int end = detailInfo.IndexOf("Visas Ã¤ven pÃ¥ tv");
+                int length = end - start;
+                string temp = detailInfo.Substring(start, length);
+                int start1 = temp.IndexOf("<p class=\"indent-bottom--small\">");
+                int end1 = temp.IndexOf("<a href");
+                int length1 = end1 - start1;
+                string temp1 = temp.Substring(start1, length1).Trim();
+                int s = temp1.IndexOf("small\">") + 7;
+                int ed = temp1.IndexOf("</p>");
+                int l = ed - s;
+                string info = temp1.Substring(s, l).Trim();
+                var convertedInfo = Encoding.UTF8.GetString(Encoding.Default.GetBytes(info));
+                textBox1.Text = convertedInfo;
+               
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //listView1.SelectedIndices.Clear();
+        }
+    }
+}
